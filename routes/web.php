5a9a62f715579b6cdacedeceb6f9153e4e6262c3
azizhadiid <azizalhadiid88@gmail.com>
2025-07-15 10:27:58 +0700

    Test Portfolio

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [WelcomeController::class, 'index']);
 Route::get('/detail/{id}', [WelcomeController::class, 'show'])->name('portfolio.detail');
 
-// Route::resource('portfolio', PortfolioController::class);
+Route::resource('portfolio', PortfolioController::class);
 
 Route::post('/', [KontakController::class, 'kirim'])->name('kontak.kirim');
